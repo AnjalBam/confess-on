aab763be8eafc1a8502bbfce6ae59026987fd647
Anjal Bam <anjalbam81@gmail.com>
2022-05-28 11:29:13 +0545

    [feat] reorganize tests

--- client/components/Button/Button.spec.tsx
+++ client/components/Button/Button.spec.tsx
@@ -1,45 +1,49 @@
-import Button from '.';
-import {screen, render} from '@testing-library/react'
- 
-describe('Test Button', () => { 
-    it('should render properly', () => {
-        render(<Button>Test Button</Button>)
-        const buttonText = screen.getByTestId('custom-button')
+import Button from ".";
+import { screen, render } from "@testing-library/react";
 
-        expect(buttonText).toBeInTheDocument();
-        expect(buttonText).toHaveTextContent('Test Button');
-        expect(buttonText).toHaveAttribute('type', 'button');
-        expect(buttonText).toHaveClass('bg-primary');
-    })
+describe("Test Button", () => {
+    describe("Test default button", () => {
+        it("should render properly", () => {
+            render(<Button>Test Button</Button>);
+            const buttonText = screen.getByTestId("custom-button");
 
-    it("should render the primary button", () => {
-        render(<Button primary>Test Button</Button>)
-        const button = screen.getByTestId('custom-button')
+            expect(buttonText).toBeInTheDocument();
+            expect(buttonText).toHaveTextContent("Test Button");
+            expect(buttonText).toHaveAttribute("type", "button");
+            expect(buttonText).toHaveClass("bg-primary");
+        });
+    });
 
-        expect(button).toBeInTheDocument();
-        expect(button).toHaveClass('bg-primary');
-    }) 
-    it("should render the secondary button", () => {
-        render(<Button secondary>Test Button</Button>)
-        const button = screen.getByTestId('custom-button')
+    describe("Test Button variants", () => {
+        it("should render the primary button", () => {
+            render(<Button primary>Test Button</Button>);
+            const button = screen.getByTestId("custom-button");
 
-        expect(button).toBeInTheDocument();
-        expect(button).toHaveClass('bg-secondary');
-    })
+            expect(button).toBeInTheDocument();
+            expect(button).toHaveClass("bg-primary");
+        });
+        it("should render the secondary button", () => {
+            render(<Button secondary>Test Button</Button>);
+            const button = screen.getByTestId("custom-button");
 
-    it("should render the danger button", () => {
-        render(<Button danger>Test Button</Button>)
-        const button = screen.getByTestId('custom-button')
+            expect(button).toBeInTheDocument();
+            expect(button).toHaveClass("bg-secondary");
+        });
 
-        expect(button).toBeInTheDocument();
-        expect(button).toHaveClass('bg-danger');
-    }) 
+        it("should render the danger button", () => {
+            render(<Button danger>Test Button</Button>);
+            const button = screen.getByTestId("custom-button");
 
-    it("should render the success button", () => {
-        render(<Button success>Test Button</Button>)
-        const button = screen.getByTestId('custom-button')
+            expect(button).toBeInTheDocument();
+            expect(button).toHaveClass("bg-danger");
+        });
 
-        expect(button).toBeInTheDocument();
-        expect(button).toHaveClass('bg-success');
-    }) 
- })
+        it("should render the success button", () => {
+            render(<Button success>Test Button</Button>);
+            const button = screen.getByTestId("custom-button");
+
+            expect(button).toBeInTheDocument();
+            expect(button).toHaveClass("bg-success");
+        });
+    });
+});
