93c1d7eb53b5ab810adf9057b5da8c8ab7c4c85c
Anjal Bam <anjalbam81@gmail.com>
2022-06-16 07:07:07 +0545

    [fix] fix bug to retain previous state on rerender

--- client/context/auth-context.tsx
+++ client/context/auth-context.tsx
@@ -14,9 +14,7 @@ const AuthContextProvider: React.FC<any> = ({children}) => {
     const [isLoggedIn, setIsLoggedIn] = React.useState<null | boolean>(null);
     useEffect(() => {
         const token = cookies.get('token');
-        if (token) {
-            setIsLoggedIn(true);
-        }
+        token ? setIsLoggedIn(true) : setIsLoggedIn(false);
     }, [])
   return (
     <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
